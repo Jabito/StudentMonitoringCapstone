e652fbba989ee9699fddd2a6ec111694483a12c8
Cisco-king <fifaquino@gmail.com>
2018-02-02 14:34:02 +0800

    sms update

--- src/main/java/com/capstone/jmt/entity/Parent.java
+++ src/main/java/com/capstone/jmt/entity/Parent.java
@@ -41,13 +41,6 @@ public class Parent implements Serializable{
     private String email;
 
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
     public Boolean getSmsNotif() {
         return smsNotif;
