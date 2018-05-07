f528b8378996586aa404317afcf2ecb0a922534f
JabitoCodes <jabito.javier@gmail.com>
2018-05-07 11:22:50 +0800

    pushing additional pages

--- src/main/java/com/capstone/jmt/controller/MainWebController.java
+++ src/main/java/com/capstone/jmt/controller/MainWebController.java
@@ -333,6 +333,13 @@ public class MainWebController {
         return "guidanceReport";
     }
 
+    @RequestMapping(value = "/summaryReport", method = RequestMethod.GET)
+    public String summaryReport(Model model) {
+
+
+        return "summaryReport";
+    }
+
     @RequestMapping(value = "/getAttendanceLogsDetails", method = RequestMethod.GET)
     public ResponseEntity<?> getAttendanceLogsDetails(@RequestParam(value = "studId") String studId) {
         HashMap<String, Object> response = new HashMap<>();
